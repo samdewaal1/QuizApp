80e8a7f6e20b3bc54b51ef691702de6aa386d977
samdewaal1 <74538378+samdewaal1@users.noreply.github.com>
2023-06-09 12:02:45 +0200

    Add function for adding a pin on the map image

--- quizapp/src/App.js
+++ quizapp/src/App.js
@@ -1,14 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+function LandingPage() {
+  const [gameCode, setGameCode] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // Check if game code is valid
+    // Redirect to game page if it is
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter game code:
+        <input type="text" value={gameCode} onChange={event => setGameCode(event.target.value)} />
+      </label>
+      <button type="submit">Join game</button>
+    </form>
+  );
+}
+
+function GamePage() {
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const questions = [
+    {
+      question: 'What is the capital of France?',
+      choices: ['Paris', 'London', 'Berlin', 'Madrid'],
+      correctAnswer: 'Paris'
+    },
+    {
+      question: 'What is the largest planet in our solar system?',
+      choices: ['Jupiter', 'Saturn', 'Neptune', 'Uranus'],
+      correctAnswer: 'Jupiter'
+    },
+    // Add more questions here
+  ];
+
+  function handleAnswerSubmit(answer) {
+    const question = questions[questionIndex];
+    if (answer === question.correctAnswer) {
+      setScore(score + 1);
+    }
+    setQuestionIndex(questionIndex + 1);
+  }
+
+  if (questionIndex >= questions.length) {
+    return (
+      <div>
+        <p>Final score: {score}</p>
+        <button onClick={() => window.location.reload()}>Play again</button>
+      </div>
+    );
+  }
+
+  const question = questions[questionIndex];
+  return (
+    <div>
+      <p>{question.question}</p>
+      {question.choices.map(choice => (
+        <button key={choice} onClick={() => handleAnswerSubmit(choice)}>{choice}</button>
+      ))}
+      <p>Score: {score}</p>
+    </div>
+  
+  );
+      
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Test
-        </p>
-      </header>
+    <div>
+      <LandingPage />
+      <GamePage />
     </div>
   );
 }
---
+++ quizapp/src/MapScreen.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './MapScreen.css';
+
+const MapScreen = () => {
+  const [circlePosition, setCirclePosition] = useState({ x: 0, y: 0 });
+
+  const handleMapClick = (event) => {
+    const rect = event.target.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    setCirclePosition({ x, y });
+  };
+
+  const handleSaveCircle = () => {
+    // Save circlePosition to database or local storage
+    console.log('Circle position saved:', circlePosition);
+  };
+
+  return (
+    <div className="map-screen">
+      <h2 className="map-screen__title">Circle Map</h2>
+      <div className="map-screen__map-container">
+        <img
+          src="map.png"
+          alt="Map"
+          className="map-screen__map-image"
+          onClick={handleMapClick}
+        />
+        <div
+          className="map-screen__circle"
+          style={{
+            left: circlePosition.x,
+            top: circlePosition.y,
+          }}
+        />
+      </div>
+      <button className="map-screen__save-button" onClick={handleSaveCircle}>
+        Save Circle
+      </button>
+    </div>
+  );
+};
+
+export default MapScreen;
