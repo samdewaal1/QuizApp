3375e1e15db78473e062fb51d85e25d7ca71099d
Rav1sh <ravishsomai808@gmail.com>
2023-06-16 11:55:14 +0200

    Update
    Update

--- quizapp/src/HomeScreen.js
+++ quizapp/src/HomeScreen.js
@@ -1,32 +1,39 @@
-import React, { useState, Image } from 'react';
+import React from 'react';
 import './HomeScreen.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+library.add(faCheckSquare);
 
 const HomeScreen = () => {
     return (
-        <body>
-            <div id="container">
-                <div id="menu">
-
-                 
-                    <ul>
-                        <li><span className="menu-icon"></span>Home</li>
-                        <li><span className="menu-icon"></span>Tutorial</li>
-                        <li><span className="menu-icon"></span>Divisies</li>
-                    </ul>
-                </div>
-                <div id="content">
-                    <h1>Welkom</h1>
-                    <p>Welkom bij de training van crisismanagement en terreur. Hier zullen we gaan oefenen met het doorlopen van high risk situaties en de risico’s, valkuilen en obstakels die hierbij komen kijken. Start de cursus, of volg een tutorial voor een uitleg van de cursus.</p>
-
-                    <h1 class="cursuss">Cursussen</h1>
-                    <h3>Cursus Crisismanagement en terreur</h3>
-                    <p>Training over Crisismanagement en terreur.</p>
-
-                    <button>Start</button>
-                </div>
+        <div id="container">
+            <div id="menu">
+                <FontAwesomeIcon icon="fa-sharp fa-solid fa-user" />
+                <ul>
+                    <li><span className="menu-icon"></span>Home</li>
+                    <li><span className="menu-icon"></span>Tutorial</li>
+                    <li><span className="menu-icon"></span>Divisies</li>
+                </ul>
             </div>
-        </body>
+            <div id="content">
+                <h1>Welkom</h1>
+                <div className="description">
+                    <p>Welkom bij de training van crisismanagement en terreur. Hier zullen we gaan oefenen met het doorlopen van high risk situaties en de risico’s, valkuilen en obstakels die hierbij komen kijken. Start de cursus, of volg een tutorial voor een uitleg van de cursus.</p>
+                </div>
+                <h1 className="cursuss">Cursussen</h1>
+                <h3>Cursus Crisismanagement en terreur</h3>
+                <p>Training over Crisismanagement en terreur.</p>
+
+                <div className="input-container">
+                    <p>Eenheid Nummer</p>
+                    <input type="text" placeholder="Bijvoorbeeld 41.02.1" />
+                </div>
+
+                <button>Start</button>
+            </div>
+        </div>
     );
 };
 
